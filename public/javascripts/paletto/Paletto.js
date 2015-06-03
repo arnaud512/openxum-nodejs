318ce3ed49e32d4e1cc5844a3dc1328dfe00ba33
Dupuy Arnaud <arnaudupuy62@gmail.com>
2015-06-03 05:47:08 -0700

    Creation de l'architecture client (fichiers vides) et initialisation du fichier Paletto.js (close #6)

---
+++ public/javascripts/paletto/Paletto.js
@@ -0,0 +1,36 @@
+// namespace Paletto
+var Paletto = { };
+
+Pentago.load = function (callback) {
+    $.getScript('/javascripts/utils/utils.js', function () {
+        $.getScript('/javascripts/MCTS/MCTS.js', function () {
+            $.getScript('/javascripts/MCTS/Node.js', function () {
+                $.getScript('/javascripts/MCTS/Player.js', function () {
+                    $.getScript('/javascripts/openxum/OpenXum.js', function () {
+                        $.getScript('/javascripts/openxum/Manager.js', function () {
+                            $.getScript('/javascripts/openxum/RemotePlayer.js', function () {
+                                $.getScript('/javascripts/openxum/MCTSPlayer.js', function () {
+                                    $.getScript('/javascripts/openxum/RestWebServicePlayer.js', function () {
+                                        $.getScript('/javascripts/openxum/AIManager.js', function () {
+                                            $.getScript('/javascripts/openxum/GamePage.js', function () {
+                                                $.getScript('/javascripts/paletto/Engine.js', function () {
+                                                    $.getScript('/javascripts/paletto/Manager.js', function () {
+                                                        $.getScript('/javascripts/paletto/Gui.js', function () {
+                                                            $.getScript('/javascripts/paletto/RandomPlayer.js', function () {
+                                                                callback();
+                                                            });
+                                                        });
+                                                    });
+                                                });
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+};
