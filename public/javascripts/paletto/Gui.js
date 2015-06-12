53ad3c2d4770c8a4b0afda6235ebba2e521cda25
DHollanderGregg <greggdh@hotmail.com>
2015-06-12 10:56:50 +0000

    Ajout des animations et des selections #8

--- public/javascripts/paletto/Gui.js
+++ public/javascripts/paletto/Gui.js
@@ -7,6 +7,7 @@ Paletto.Gui = function (c, e, l, g) {
     var _color;
     var _local;
     var _gui;
+    var opponentPresent = l;
 
     var _canvas;
     var _context;
@@ -28,6 +29,11 @@ Paletto.Gui = function (c, e, l, g) {
     var _button_hover=false;
     var _button_clicked = false;
     var _color_piece_played = null;
+
+    var _moving_piece_x=null;
+    var _moving_piece_y=null;
+    var _id=null;
+    var is_animating=false;
 
     // public methods
     // get color of player used GUI
@@ -91,7 +97,7 @@ Paletto.Gui = function (c, e, l, g) {
             for (j = 0; j < 6; ++j) {
                 tmp_piece_color= _engine.get_piece_color_from_x_y(i,j);
                 draw_hole(_offsetX + (i + 0.5) * _deltaX, _offsetY + (j -0.2) * _deltaY-9, _deltaX / 2.5);
-                draw_piece_colored(_offsetX + (i + 0.5) * _deltaX, _offsetY + (j -0.2) * _deltaY-9, _deltaX / 1.8, tmp_piece_color);
+                draw_piece_colored(_offsetX + (i + 0.5) * _deltaX, _offsetY + (j -0.2) * _deltaY-9, _deltaX / 1.8, tmp_piece_color, false);
             }
         }
         // Ligne
@@ -119,16 +125,16 @@ Paletto.Gui = function (c, e, l, g) {
                 if (i == 1) deca = 0.3;
                 else deca = 0;
                 if (i == 2) deca = 0.6;
-                draw_piece_colored(_offsetX + (i - 0.4 + decalage - deca) * _deltaX, _offsetY + (j + 5.82 - decalage2 ) * _deltaY, _deltaX / 1.8, cpt);
+                var is_bordered = (_engine.current_color() == Paletto.Color.JOUEUR_1);
+                draw_piece_colored(_offsetX + (i - 0.4 + decalage - deca) * _deltaX, _offsetY + (j + 5.82 - decalage2 ) * _deltaY, _deltaX / 1.8, cpt, is_bordered);
 
                 // Aficher le nb de piece
+
                 var tmp = _engine.get_taken_color(Paletto.Color.JOUEUR_1, cpt);
                 _context.fillStyle = "#989898";
-                _context.font="26px Verdana";
-                _context.lineWidth = 3.2;
-                _context.strokeStyle = "#757D75";
+                _context.font="900 30px Arial";
                 _context.beginPath();
-                _context.strokeText(tmp,_offsetX + (i -0.484+ decalage-deca) * _deltaX,_offsetY + (j +5.93- decalage2 ) * _deltaY);
+                _context.fillText(tmp,_offsetX + (i -0.51+ decalage-deca) * _deltaX,_offsetY + (j +5.97- decalage2 ) * _deltaY);
                 _context.closePath();
                 _context.fill();
                 _context.stroke();
@@ -159,17 +165,16 @@ Paletto.Gui = function (c, e, l, g) {
                 x = cpt;
                 if (j==0) x--;
                 else x ++;
-                draw_piece_colored(_offsetX + (i +4.67+ decalage-deca) * _deltaX, _offsetY + (j +5.82- deca2 ) * _deltaY, _deltaX / 1.8, x);
+                var is_bordered = (_engine.current_color() == Paletto.Color.JOUEUR_2);
+                draw_piece_colored(_offsetX + (i +4.67+ decalage-deca) * _deltaX, _offsetY + (j +5.82- deca2 ) * _deltaY, _deltaX / 1.8, x, is_bordered);
 
 
                 // Aficher le nb de piece
                 var tmp = _engine.get_taken_color(Paletto.Color.JOUEUR_2, x);
                 _context.fillStyle = "#989898";
-                _context.font="26px Verdana";
-                _context.lineWidth = 3.2;
-                _context.strokeStyle = "#757D75";
+                _context.font="900 30px Arial";
                 _context.beginPath();
-                _context.strokeText(tmp,_offsetX + (i +4.58+ decalage-deca) * _deltaX, _offsetY + (j +5.925- deca2 ) * _deltaY, _deltaX / 1.8);
+                _context.fillText(tmp,_offsetX + (i +4.56+ decalage-deca) * _deltaX, _offsetY + (j +5.96- deca2 ) * _deltaY, _deltaX / 1.8);
                 _context.closePath();
                 _context.fill();
                 _context.stroke();
@@ -216,7 +221,7 @@ Paletto.Gui = function (c, e, l, g) {
 
         _context.beginPath();
         gr.addColorStop(1, '#bfbfbf');
-        gr.addColorStop(0, '#757D75');
+        gr.addColorStop(0, '#c0c0c0');
         _context.fillStyle = gr;
         _context.arc(x, y, width / 2, 0.0, 2 * Math.PI, false);
         _context.closePath();
@@ -236,12 +241,20 @@ Paletto.Gui = function (c, e, l, g) {
     };
 
     //piece
-    var draw_piece_colored = function (x, y, width, nb_color) {
+    var draw_piece_colored = function (x, y, width, nb_color, is_bordered) {
         var gr = _context.createRadialGradient(x, y, width / 3, x, y, width);
         if (nb_color != -1) {
             _context.lineWidth = 1;
             _context.strokeStyle = "#757D75";
             _context.beginPath();
+            if (_color_piece_played ==nb_color && is_bordered){
+                _context.lineWidth = 4;
+                _context.strokeStyle = "#757D75";
+            }
+            else {
+                _context.lineWidth = 1;
+                _context.strokeStyle = "#757D75";
+            }
             switch (nb_color) {
                 case 0:
                     gr.addColorStop(1, '#000000');
@@ -333,7 +346,7 @@ Paletto.Gui = function (c, e, l, g) {
 
 
     var onMove = function (event) {
-        if (_engine.current_color()=== _color || _gui){
+        if ((_engine.current_color()=== _color || _gui)&& is_animating == false){
             var pos = getClickPosition(event);
             var xy = get_pieces_position(pos.x,pos.y);
             if(xy) {
@@ -382,7 +395,126 @@ Paletto.Gui = function (c, e, l, g) {
 
     var calc_tri_rea = function(x1, y1, x2, y2, x3, y3){
         return ((x1 - x3) * (y2 - y3)) - ((x2 - x3) * (y1 - y3));
-    }
+    };
+
+    // Animation
+    var animate = function (color, player, pos_x, pos_y) {
+        is_animating = true;
+        //console.log("Couleur "+color+"\nJoueur " +player+ "\nPosition x "+pos_x+"\nPosition y "+pos_y);
+        //recup position init
+        var ini = initial_position();
+        console.log("Couleur "+color+"\nJoueur " +player+"\n cible x"+ ini.x+"\ncible y" + ini.y);
+        //recup position cible
+        var tar = target_position (player, color);
+        //console.log("Couleur "+color+"\nJoueur " +player+"\n target x"+ tar.x+"\ntarget y" + tar.y);
+        _moving_piece_x = ini.x;
+        _moving_piece_y = ini.y;
+       _id= setInterval(function() {
+           move_piece(ini.x, ini.y, tar.x, tar.y, color)
+       }, 20);
+
+    };
+
+    var move_piece = function(x1, y1, x2, y2, color){
+        var run = 10;
+        _manager.redraw();
+        var dist_x = Math.abs(x2-x1);
+        var dist_y = Math.abs(y2-y1);
+        var diago = Math.sqrt(dist_x*dist_x + dist_y*dist_y);
+        //console.log(dist_x+"\n"+dist_y+"\n"+diago);
+
+        draw_piece_colored(_moving_piece_x, _moving_piece_y, _deltaX / 1.8, color, false);
+        if (x1< x2){
+            _moving_piece_x += (dist_x/diago) * run;
+        }
+        else{
+            _moving_piece_x -= (dist_x/diago) * run;
+        }
+        if (y1 <y2){
+            _moving_piece_y += (dist_y/diago) * run;
+        }
+        else {
+            _moving_piece_y -= (dist_y/diago) * run;
+        }
+       /* _moving_piece_x += (dist_x/diago) * run;
+        _moving_piece_y += (dist_y/diago) * run;*/
+        //console.log("\n jlo 2 =>"+_moving_piece_x);
+        if (_moving_piece_y>=y2 )  {
+            clearInterval(_id);
+            is_animating = false;
+            _manager.redraw();
+        }
+    };
+
+
+
+    // recup position initiale de la couleur jouée
+    var initial_position = function (){
+        var tar_x = _offsetX + (_x_pos + 0.5) * _deltaX;
+        var tar_y =  _offsetY + (_y_pos -0.2) * _deltaY-9;
+        return {x:tar_x, y:tar_y};
+    };
+
+    // recup position cible de la couleur jouée
+    var target_position = function (player, color){
+        var i=0, j=0, cpt = 0;
+        var decalage = 0, deca = 0, decalage2=0, deca2 = 0;
+        var x = 0;
+        var tar_x = null;
+        var tar_y = null;
+        //joueur 1
+        if (player ==0){
+            for (i = 0; i < 3; ++i) {
+                for (j = 0; j < 2; ++j) {
+                    if (j == 1) decalage = 0.34;
+                    else decalage = 0;
+                    if (j == 1) decalage2 = 0.4;
+                    else decalage2 = 0;
+                    if (i == 1) deca = 0.3;
+                    else deca = 0;
+                    if (i == 2) deca = 0.6;
+                    if (color ==cpt){
+                        tar_x = _offsetX + (i - 0.4 + decalage - deca) * _deltaX;
+                        tar_y = _offsetY + (j + 5.82 - decalage2 ) * _deltaY
+                        console.log("x : "+tar_x);
+                        console.log("\ny : "+tar_y);
+                        return {x:tar_x, y:tar_y};
+
+                    }
+                    cpt++;
+                }
+            }
+        }
+        // joueur 2
+        else {
+            cpt =5;
+
+            // case
+            for (i = 0; i < 3; ++i) {
+                for (j = 0; j < 2; ++j) {
+                    if(j == 0) decalage = 0.34;
+                    else decalage =0;
+                    if (j==1) deca2 = 0.4;
+                    else deca2=0;
+                    if (i ==1) deca = 0.3;
+                    else deca = 0;
+                    if (i==2) deca = 0.6;
+
+                    x = cpt;
+                    if (j==0) x --;
+                    else x ++;
+                    if (color ==x){
+                        tar_x = _offsetX + (i +4.67+ decalage-deca) * _deltaX;
+                        tar_y = _offsetY + (j +5.82- deca2 ) * _deltaY;
+                        return {x:tar_x, y:tar_y};
+
+                    }
+                    cpt--;
+                }
+
+            }
+        }
+    };
 
     // _offsetX + 6 * _deltaX -10
     // return the move that the player has made
@@ -408,12 +540,12 @@ Paletto.Gui = function (c, e, l, g) {
     // apply a move and animate
     // if no animation, call manager.play
     this.move = function (move, color) {
-        //_manager.play();
+        _manager.play();
     };
 
     // ready is called when opponent is present (online case)
     this.ready = function (r) {
-        var opponentPresent = r;
+        opponentPresent = r;
         if (_manager) {
             _manager.redraw();
         }
@@ -469,7 +601,7 @@ Paletto.Gui = function (c, e, l, g) {
     // if interaction is click action, verify if the phase of engine is equal to YYYY (a phase of game)
     // and differents interactions are realized
     var onClick = function (event) {
-        if (_engine.current_color()=== _color || _gui){
+        if ((_engine.current_color()=== _color || _gui) && is_animating==false){
             var pos = getClickPosition(event);
             var xy = get_pieces_position(pos.x,pos.y);
             if(xy){
@@ -493,6 +625,7 @@ Paletto.Gui = function (c, e, l, g) {
                                     _x_pos = xy.x;
                                     _y_pos = xy.y;
                                     _color_pos = tmp_color_piece;
+                                    animate(_color_piece_played, _engine.current_color(), pos.x, pos.y);
                                     _manager.play();
                                 }
                             }
